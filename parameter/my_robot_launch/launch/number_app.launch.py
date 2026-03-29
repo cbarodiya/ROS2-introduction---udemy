728578f3423ba3624e7c5ca9d83ac2e849c6edde
chetanya barodiya <chetanyabarodiya@gmail.com>
2026-03-29 08:00:00 +0530

    Add launch file for number publisher and counter

--- parameter/my_robot_launch/launch/number_app.launch.py
+++ parameter/my_robot_launch/launch/number_app.launch.py
@@ -1,1 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    
+
+    number_pubisher = Node(
+        package="my_py_pkg",
+        executable="number_publisher",
+        name="my_number_publisher",
+        remappings=[("/number", "/my_number")],
+        parameters=[
+            {"number": 12},
+            {"timer_period": 1.4}
+        ]
+    )
+
+    number_counter = Node(
+        package="my_cpp_pkg",
+        executable="number_counter",
+        name="my_number_counter",
+        remappings=[("/number", "/my_number")]
+    )
+
+    ld.add_action(number_pubisher)
+    ld.add_action(number_counter)
+
+    return ld
